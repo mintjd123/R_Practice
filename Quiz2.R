741188a4337c16d16536693bf23898ae0498a422
Anonymous <80660607+mintjd123@users.noreply.github.com>
2021-06-17 11:13:52 +0900

    Quiz2.R
    제품에 사과, 딸기, 수박, 그에 맞는 가격과 판매량이 들어가 있는 데이터프레임을 만들고 가격과 판매량의 전체 평균을 구하라.
    
    결과 값
    > fruit_info
      제품 가격 판매량
    1 사과 1800     24
    2 딸기 1500     38
    3 수박 3000     13
    
    > mean(fruit_info$가격)
    [1] 2100
    > mean(fruit_info$판매량)
    [1] 25

---
+++ Quiz2.R
@@ -0,0 +1,8 @@
+fruit_info <- data.frame(제품 = c("사과", "딸기", "수박"),
+                         가격 = c(1800, 1500, 3000),
+                         판매량 = c(24, 38, 13))
+
+fruit_info
+
+mean(fruit_info$가격)
+mean(fruit_info$판매량)
