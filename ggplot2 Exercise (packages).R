c8725c2cb27835bcca3d0ec08867d44de5ce91ac
Anonymous <80660607+mintjd123@users.noreply.github.com>
2021-06-17 10:55:59 +0900

    Create ggplot2 Exercise (packages).R

---
+++ ggplot2 Exercise (packages).R
@@ -0,0 +1,12 @@
+x <- c("a", "a", "b", "c")
+qplot(x)
+
+qplot(data = mpg, x = hwy)
+qplot(data = mpg, x = cty)
+qplot(data = mpg, x = drv, y = hwy)
+qplot(data = mpg, x = drv, y = hwy, geom = "line")
+qplot(data = mpg, x = drv, y = hwy, geom = "boxplot")
+qplot(data = mpg, x = drv, y = hwy, geom = "boxplot", colour = drv)
+
+
+?qplot
